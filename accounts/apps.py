256a4a3cfb2f9602c905534e0b86f4c83bd26bfa
Dennys <dennyssolis@gmail.com>
2020-11-16 21:55:35 -0500

    .

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
+from django.contrib.auth.models import Group
 
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
+
+    def ready(self):
+
+        Group.objects.get_or_create(name='Mechanics')
+        Group.objects.get_or_create(name='Office')
